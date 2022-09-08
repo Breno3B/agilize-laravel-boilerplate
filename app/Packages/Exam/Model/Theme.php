466bdb5c34ac4973110bf4ded332d9fece2f71de
Breno R. Magalhães <breno.magalhaes@agilize.com.br>
2022-09-07 23:53:38 -0300

    Create Theme entity

---
+++ app/Packages/Exam/Model/Theme.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Packages\Exam\Model;
+
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Illuminate\Support\Str;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="theme")
+ */
+class Theme
+{
+    use TimestampableEntity;
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="uuid", unique=true)
+     */
+    protected string $id;
+
+    /**
+     * @ORM\Column(type="string", unique=true, length=64)
+     */
+    protected string $name;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    protected string $description;
+
+    /**
+     * @param  string  $name
+     * @param  string  $description
+     */
+    public function __construct(string $name, string $description)
+    {
+        $this->id = Str::uuid()->toString();
+        $this->name = $name;
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param  string  $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param  string  $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+}
