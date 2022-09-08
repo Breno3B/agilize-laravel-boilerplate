3d63a7ab34380527afd6637b9dcc58e04da9d897
Breno R. Magalhães <breno.magalhaes@agilize.com.br>
2022-09-07 21:27:41 -0300

    Create entity manager factory

---
+++ app/Helper/EntityManagerFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Helper;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\ORMSetup;
+
+class EntityManagerFactory
+{
+    /**
+     * @return EntityManager
+     * @throws ORMException
+     */
+    public static function getEntityManager(): EntityManager
+    {
+        $rootDir = __DIR__ . '/../../../';
+
+        $config = ORMSetup::createAnnotationMetadataConfiguration(
+            [$rootDir . '/app'],
+            true
+        );
+        $connection = [
+            'driver' => 'pdo_pgsql',
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT'),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD')
+        ];
+
+        return EntityManager::create($connection, $config);
+    }
+}
