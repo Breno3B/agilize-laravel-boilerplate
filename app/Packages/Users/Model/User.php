2b93f046d00b10d41a0d358b67e2a1e900d71141
Breno R. Magalhães <breno.magalhaes@agilize.com.br>
2022-09-07 23:36:30 -0300

    Create User entity

---
+++ app/Packages/Users/Model/User.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Packages\Users\Model;
+
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Illuminate\Support\Str;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User
+{
+    use TimestampableEntity;
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="uuid", unique=true)
+     */
+    protected string $id;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    protected string $name;
+
+    /**
+     * @param  string  $name
+     */
+    public function __construct(string $name)
+    {
+        $this->id = Str::uuid()->toString();
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param  string  $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+}
