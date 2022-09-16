2804cac425ad766b8e1fdd3142e8861066a2ef48
Breno R. Magalhães <breno.magalhaes@agilize.com.br>
2022-09-15 23:43:35 -0300

    adjust construct indentation

--- app/Packages/Exams/Model/Theme.php
+++ app/Packages/Exams/Model/Theme.php
@@ -35,8 +35,7 @@ class Theme
      * @param  string  $name
      * @param  string  $description
      */
-    public function __construct(string $name, string $description)
-    {
+    public function __construct(string $name, string $description) {
         $this->id = Str::uuid()->toString();
         $this->name = $name;
         $this->description = $description;
